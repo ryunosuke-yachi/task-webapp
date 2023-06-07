a3a0b02d75f2519c694446ef493b65b252bb2f11
ryunosuke_yachi <120090113+ryunosuke-yachi@users.noreply.github.com>
2023-06-07 14:28:00 +0900

    Update web.php

--- my-project/routes/web.php
+++ my-project/routes/web.php
@@ -16,3 +16,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/show', function () {
+    return 'タスク一覧画面';
+});
+
+Route::get('/register', function () {
+    return 'タスク登録画面';
+});
+
+Route::get('/edit', function () {
+    return 'タスク編集画面';
+});
+
+Route::get('/detail', function () {
+    return 'タスク詳細画面';
+});
+
+Route::get('/remove', function () {
+    return 'タスク削除画面';
+});
+
+Route::get('/sort', function () {
+    return 'ソート画面';
+});
+
+Route::get('/calendar', function () {
+    return 'カレンダー画面';
+});
