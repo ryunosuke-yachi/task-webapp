a626cc12abb222acb0a52d745ad815464a43a9db
ryunosuke_yachi <120090113+ryunosuke-yachi@users.noreply.github.com>
2023-07-28 17:55:12 +0900

    css反映

--- my-project/resources/views/register.blade.php
+++ my-project/resources/views/register.blade.php
@@ -3,48 +3,50 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="{{asset('css/register.css')}}">
+        <link rel="stylesheet" href="{{asset('/css/register.css')}}">
     </head>
     <body>
-        <header>
-            <h1>登録画面</h1>
-        </header>
 
         <main>
             <form action="" method="post">
-                <p>タスク名</p>
-                <input type="text" />
 
-                <p>進捗</p>
-                <select name="start">
-                <option value="notstart">未着手</option>
-                <option value="start">作業中</option>
-                <option value="clear">完了</option>
-                </select>
+                <div class="register">
+                    <input type="text" name="name" value=""placeholder="タスク名" />
 
-                <p>重要度</p>
-                <select name="start">
-                <option value="notstart">低</option>
-                <option value="start">中</option>
-                <option value="clear">高</option>
-                </select>
+                    <br><input type="text" value=""placeholder="ジャンル"/>
 
-                <p>締め切り日</p>
-                <input type="date" />
+                    <div class="progressmenu">
+                        <select name="progress" id="progress">
+                            <option value="">進捗</option>
+                            <option value="notstart">未着手</option>
+                            <option value="start">作業中</option>
+                            <option value="clear">完了</option>
+                        </select>
+                    </div>
 
-                <p>ジャンル</p>
-                <input type="text" />
+                    <div class="severitymenu">
+                        <select name="severity" id="severity">
+                            <option value="">優先度</option>
+                            <option value="notstart">低</option>
+                            <option value="start">中</option>
+                            <option value="clear">高</option>
+                        </select>
+                    </div>
 
-                <p>目標日</p>
-                <input type="date" />
+                    <p class="registertxt">締め切り日</p>
+                    <input type="date" />
 
-                <button type="submit">Submit</button>
+                    <p class="registertxt">目標日</p>
+                    <input type="date" />
+
+                    <br><button type="submit">登録</button>
+                </div>
+
+
             </form>
-
-
-
-
-            <br><button onclick="location.href='./show'">戻る</button>
+            <div class="back">
+                <br><button onclick="location.href='./show'">戻る</button>
+            </div>
         </main>
 
 
